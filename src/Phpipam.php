235e04cd4115c4e6611906a160be9766df3f3be1
Bart Vrancken <bart@abuse.io>
2015-12-30 12:43:07 +0100

    add docblocks

---
+++ src/Phpipam.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace AbuseIO\Findcontact;
+
+//use AbuseIO\Jobs\FindContact;
+//use AbuseIO\Models\Contact;
+
+/**
+ * Class Phpipam
+ * @package AbuseIO\Findcontact
+ */
+class Phpipam
+{
+}
